b8103468fc0e989453cf40f9f4ee4af4b4647384
xTres1985 <boguslaw.borozynski@gmail.com>
2020-03-30 23:23:09 +0200

    start

---
+++ src/main/java/com/mydelka/mydelka/MydelkaApplication.java
@@ -0,0 +1,13 @@
+package com.mydelka.mydelka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydelkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydelkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mydelka/mydelka/MydelkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydelka.mydelka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydelkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
